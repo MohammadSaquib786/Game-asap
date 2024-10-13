b68273111e1764919ab307da2da061fcbdfa0be8
MohammadSaquib786 <mohdsaquib7068@gmail.com>
2024-10-13 21:32:18 +0530

    Fully

--- src/Components/Login Page/Login.js
+++ src/Components/Login Page/Login.js
@@ -7,16 +7,17 @@ function Login() {
     return (
         <div className='login-div'>
             <div className="head-login">
-                <h2 style={{color:'yellow'}}> {<SiGamejolt/>}GAME asap</h2>
+                <h2 style={{color:'yellow'}} className='h2-div'> {<SiGamejolt/>}GAME asap</h2>
+                <h4 style={{color:'yellow'}} className='h4-div'> {<SiGamejolt/>}GAME asap</h4>
                 <button onClick={() => navigate('/')} className='button-singup'>Skip Login </button>
             </div>
+          
             <div className="login-heading">
                 <h1>Log In</h1>
                 <h3>Let`s get Started!</h3>
             </div>
             <form action="" className='form'>
                 <div className="input-box">
-                    {/* <input type="text" placeholder='Name' className='input-text' /> */}
                     <input type="email" placeholder='Enter Your Email' className='input-text' />
                     <input type="password" placeholder='Password' className='input-text' />
                 </div>
